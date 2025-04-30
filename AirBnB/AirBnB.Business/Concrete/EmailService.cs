cc3f9275feffbaf3932e9f2d4289d53200ca7aa4
Humbat Yusubov <87598661+MrYusubov@users.noreply.github.com>
2025-04-30 16:38:01 +0400

    Confirm Email sent done

--- AirBnB/AirBnB.Business/Concrete/EmailService.cs
+++ AirBnB/AirBnB.Business/Concrete/EmailService.cs
@@ -15,19 +15,21 @@ public class EmailService:IEmailService
 
     public async Task SendEmailAsync(string toEmail, string subject, string body)
     {
-        var smtpClient = new SmtpClient(_configuration["Email:SmtpServer"])
+        var emailSettings = _configuration.GetSection("Email");
+
+        var smtpClient = new SmtpClient(emailSettings["SmtpServer"])
         {
-            Port = int.Parse(_configuration["Email:Port"]),
-            Credentials = new NetworkCredential(_configuration["Email:Username"], _configuration["Email:Password"]),
-            EnableSsl = true,
+            Port = int.Parse(emailSettings["Port"]),
+            Credentials = new NetworkCredential(emailSettings["Username"], emailSettings["Password"]),
+            EnableSsl = true
         };
 
         var mailMessage = new MailMessage
         {
-            From = new MailAddress(_configuration["Email:FromEmail"]),
+            From = new MailAddress(emailSettings["FromEmail"]),
             Subject = subject,
             Body = body,
-            IsBodyHtml = true,
+            IsBodyHtml = true
         };
         mailMessage.To.Add(toEmail);
 
